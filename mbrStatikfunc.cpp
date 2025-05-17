a4682697a3873bf25b9c605425c65e8dc2b07445
devitadwiindayani <devitadwiindayani@gmail.com>
2025-05-17 15:58:11 +0700

    add kelas mahasiswa

--- mbrStatikfunc.cpp
+++ mbrStatikfunc.cpp
@@ -1,3 +1,15 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class mahasiswa
+{
+private:
+    static int nim;
+
+public:
+    int id;
+    string nama;
+
+    void setID();
+    void printAll();
